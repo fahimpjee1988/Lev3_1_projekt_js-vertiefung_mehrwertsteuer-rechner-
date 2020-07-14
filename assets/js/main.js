08405ea8fe870424163b837819890bf04aac21ac
fahimpjee1988 <adjmal.ra@gmail.com>
2020-07-14 16:05:26 +0200

    daswarschwerdu

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+function berechnen(){
+    if(document.getElementById("netto").checked){
+        console.log("netto")
+        if(document.getElementById("neuzehn").checked){
+            document.getElementById("mehrwert").innerHTML=
+        Number(document.getElementById("typein").value)*0.19
+        document.getElementById("mehrwert2").innerHTML=
+        Number(document.getElementById("typein").value)*0.19+Number(document.getElementById("typein").value)
+    
+        }else{
+            document.getElementById("mehrwert").innerHTML=
+            Number(document.getElementById("typein").value)*0.07
+            document.getElementById("mehrwert2").innerHTML=
+            Number(document.getElementById("typein").value)*0.07+Number(document.getElementById("typein").value)
+        
+        }
+        }
+    else{
+        if(document.getElementById("neuzehn").checked){
+
+
+            document.getElementById("mehrwert2").innerHTML=
+        Number(document.getElementById("typein").value)/1.19
+
+
+        document.getElementById("mehrwert").innerHTML=
+        (Number(document.getElementById("typein").value)-Number((document.getElementById("typein").value)/1.19)).toFixed(2)
+    
+        }else{
+            document.getElementById("mehrwert2").innerHTML=
+            Number(document.getElementById("typein").value)/1.07
+
+
+            document.getElementById("mehrwert").innerHTML=
+            ((Number(document.getElementById("typein").value)-Number(document.getElementById("typein").value)/1.07)).toFixed(2)
+        
+        }
+    }
+}
